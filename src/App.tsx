cc69612fd7853fd0912445a36065d487945caf1f
sammu1991 <samata.karka@gmail.com>
2024-01-28 19:21:14 -0600

    Room Ranger App Init Project Setup

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Room Ranger App Init Project Setup
         </a>
       </header>
     </div>
